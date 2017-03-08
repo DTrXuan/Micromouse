a4be31d1d21d6e0c78baaa9f7e9fdeb95f40d9bf
Pedro Pereira <pmh.pereira@gmail.com>
2017-03-08 09:02:16 +0000

    Added distance threshold in editor

--- Unity/Assets/Scripts/MazeEditor.cs
+++ Unity/Assets/Scripts/MazeEditor.cs
@@ -1,43 +1,47 @@
 ﻿using System;
 using System.Collections;
 using UnityEditor;
+using UnityEditor.SceneManagement;
 using UnityEngine;
 
 public class MazeEditor : MonoBehaviour
 {
-	private static bool isEditing;
+	public bool isEditing;
+    public float distanceToWallThreshold;
 
 	public void Update()
 	{
-		if (isEditing && Input.GetMouseButtonDown(0))
+		if (isEditing && (Input.GetMouseButton(0) || Input.GetMouseButton(1)))
 		{
 			RaycastHit hit;
 			Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
 			if (Physics.Raycast(ray, out hit, 100.0f))
 			{
-				ToggleWall(hit);
+				ToggleWall(hit, insert: Input.GetMouseButton(0) && !Input.GetMouseButton(1));
 			}
 		}
 	}
 
-	public void ToggleWall(RaycastHit hit)
+	public void ToggleWall(RaycastHit hit, bool insert)
 	{
 		var pillar1 = GetClosestPillar(hit.point);
 		var pillar2 = GetClosestPillar(hit.point, pillar1);
 
-		Vector3 diffPillar = pillar2.position - pillar1.position;
+		Vector3 bottomLeftPosition = new Vector3(Math.Min(pillar1.position.x, pillar2.position.x), 0, Math.Min(pillar1.position.z, pillar2.position.z));
+		Vector3 topRightPosition = new Vector3(Math.Max(pillar1.position.x, pillar2.position.x), 0, Math.Max(pillar1.position.z, pillar2.position.z));
 
-		Vector2 bottomLeftPosition = new Vector2(Math.Min(pillar1.position.x, pillar2.position.x), Math.Min(pillar1.position.z, pillar2.position.z));
-		Maze.Coord bottomLeftCoord = new Maze.Coord(0, 0);
-		bottomLeftCoord.x = (int) Math.Round((bottomLeftPosition.x) * Maze.MazeSize.x / (Camera.main.transform.position.x * 2));
-		bottomLeftCoord.y = (int) Math.Round((bottomLeftPosition.y) * Maze.MazeSize.y / (Camera.main.transform.position.z * 2));
-		
-		Vector2 topRightPosition = new Vector2(Math.Max(pillar1.position.x, pillar2.position.x), Math.Max(pillar1.position.z, pillar2.position.z));
-		Maze.Coord topRightCoord = new Maze.Coord(0, 0);
-		topRightCoord.x = (int) Math.Round((topRightPosition.x) * Maze.MazeSize.x / (Camera.main.transform.position.x * 2));
-		topRightCoord.y = (int) Math.Round((topRightPosition.y) * Maze.MazeSize.y / (Camera.main.transform.position.z * 2));
+	    var distanceToWallCenter = Vector3.Distance(hit.point, (topRightPosition + bottomLeftPosition)/2);
 
-		Maze.Coord c0 = null;
+        if (distanceToWallCenter > distanceToWallThreshold)
+	        return;
+
+        Maze.Coord bottomLeftCoord = new Maze.Coord(0, 0);
+        bottomLeftCoord.x = (int)Math.Round((bottomLeftPosition.x) * Maze.MazeSize.x / (Camera.main.transform.position.x * 2));
+        bottomLeftCoord.y = (int)Math.Round((bottomLeftPosition.z) * Maze.MazeSize.y / (Camera.main.transform.position.z * 2));
+
+        Vector3 diffPillar = pillar2.position - pillar1.position;
+
+        Maze.Coord c0 = null;
 		Maze.Coord c1 = null;
 		Maze.Wall wall = Maze.Wall.None;
 
@@ -64,15 +68,15 @@ public class MazeEditor : MonoBehaviour
 
 		if (c0 != null && c1 != null)
 		{
-			if (Maze.Cells[c0.x, c0.y].HasWall(wall))
-			{
-				Maze.Cells[c0.x, c0.y].RemoveWall(wall);
-				Maze.Cells[c1.x, c1.y].RemoveWall(Maze.GetOppositeWall(wall));
-			}
-			else
+			if (insert)
 			{
 				Maze.Cells[c0.x, c0.y].InsertWall(wall);
 				Maze.Cells[c1.x, c1.y].InsertWall(Maze.GetOppositeWall(wall));
+			}
+			else
+			{
+				Maze.Cells[c0.x, c0.y].RemoveWall(wall);
+				Maze.Cells[c1.x, c1.y].RemoveWall(Maze.GetOppositeWall(wall));
 			}
 
 			MazeSerializer.ResetMazeName();
@@ -82,7 +86,7 @@ public class MazeEditor : MonoBehaviour
 
 	private GameObject[] pillars;
 
-	Transform GetClosestPillar(Vector3 clickPoint, Transform excludePillar = null)
+	private Transform GetClosestPillar(Vector3 clickPoint, Transform excludePillar = null)
 	{
 		Transform tMin = null;
 		float minDist = Mathf.Infinity;
@@ -107,25 +111,36 @@ public class MazeEditor : MonoBehaviour
 	{
 		public override void OnInspectorGUI()
 		{
-			GUI.enabled = Application.isPlaying;
-			
-			EditorGUILayout.LabelField("");
+		    var editor = (MazeEditor) target;
 
-			if (!MazeEditor.isEditing)
+            editor.distanceToWallThreshold = EditorGUILayout.Slider(new GUIContent("Distance Threshold (m)"), editor.distanceToWallThreshold, 0.01f, 0.20f);
+
+			GUI.enabled = Application.isPlaying;
+
+			if (!editor.isEditing)
 			{
 				if (GUILayout.Button("Start editing"))
 				{
 					MazeSerializer.ResetMazeName();
-					MazeEditor.isEditing = true;
+                    editor.isEditing = true;
 				}
 			}
 			else
 			{
 				if (GUILayout.Button("Stop editing"))
 				{
-					MazeEditor.isEditing = false;
+                    editor.isEditing = false;
 				}
 			}
-		}
-	}
+
+            if (!Application.isPlaying)
+            {
+                if (GUI.changed)
+                {
+                    EditorUtility.SetDirty(editor);
+                    EditorSceneManager.MarkSceneDirty(editor.gameObject.scene);
+                }
+            }
+        }
+    }
 }
